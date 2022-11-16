bb7ef79f15405fc9a946fe96600d29bfc01db946
Anastasiya Matveyenka <a.matveyenka@gmail.com>
2022-11-16 17:16:20 +0100

    Google Codelab. Android - Add a button to an app - Create an interactive Dice Roller app

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener() { rollDice() }
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+
+        // Create new Dice object with 10 sides and roll it
+        val mySecondDice = Dice(10)
+        val diceRoll2 = mySecondDice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView2)
+        resultTextView.text = diceRoll.toString()
+        val resultTextView2: TextView = findViewById(R.id.textView4)
+        resultTextView2.text = diceRoll2.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
